a68c391a9b6de987871bcacd7fcfacd78239d9ba
Jean BaMaung <jbamaung17@gmail.com>
2018-02-11 19:34:33 +0000

    added CRUD functions and delete_all function to films.rb

--- models/films.rb
+++ models/films.rb
@@ -13,5 +13,32 @@ class Film
     @price = options['price'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
+    values = [@title, @price]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
 
+  def update()
+    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM films"
+    film_list = SqlRunner.run(sql)
+    return film_list.map{|film|Film.new(film_list)}
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end 
 end
