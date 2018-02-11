db3d910cf665b6153fd90aec3031c73951d97264
Jean BaMaung <jbamaung17@gmail.com>
2018-02-11 19:24:46 +0000

    added CRUD funtions to tickets.rb

--- models/tickets.rb
+++ models/tickets.rb
@@ -9,10 +9,33 @@ class Ticket
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
-    @customer_id = options['id'].to_i
-    @film
+    @customer_id = options['customer_id'].to_i
+    @film_id = options['film_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO tickets(customer_id, film_id) VALUES ($1, $2) RETURNING id"
+    values = [@customer_id, @film_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
 
+  def self.all()
+    sql = "SELECT * FROM tickets"
+    ticket_list = SqlRunner.run(sql)
+    result = ticket_list.map {|ticket|Ticket.new(ticket_list)}
+    return result
+  end
+
+  def update()
+    sql = "UPDATE tickets SET (customer_id, film_id) = ($1, $2) WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
 
 end
