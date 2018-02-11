e2aa32ee42d3bffe9ab3541ee35b50396f981bcb
Jean BaMaung <jbamaung17@gmail.com>
2018-02-11 19:56:36 +0000

    added CRUD functions and delete_all to customers.rb

--- models/customers.rb
+++ models/customers.rb
@@ -12,6 +12,33 @@ class Customer
     @funds = options['funds'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
 
+  def update()
+    sql = "UPDATE customers (name, funds) SET ($1, $2) WHERE id = $3"
+    values = [@name, @funs, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM customers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    customer_list = SqlRunner.run(sql)
+    return customer_list.map{|customer|Customer.new(customer_list)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
 
 end
